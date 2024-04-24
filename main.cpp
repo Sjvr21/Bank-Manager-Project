6602f2d8a8768756133acaab16faf40e8b991b7a
Sergio Velez <125153495+Sjvr21@users.noreply.github.com>
2024-04-24 15:21:48 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,55 @@
+
+#include <iostream>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+int main(){
+
+    int AccNumber, money, AccMoney = 10000;
+char opcion;
+
+while(opcion != 'n'){
+
+    time_t now = time(0);
+   
+   char* dateTime = ctime(&now);
+
+
+    ofstream transfers("transactions.txt");
+    
+
+    
+    cout << "Enter the account number: ";
+    cin >> AccNumber;
+
+    cout << "Enter the ammount of money you want to transfer: ";
+    cin >> money;
+
+    if(money <= AccMoney){
+        
+         cout << "Transfer sucessful";
+
+        transfers << "You transferred $"<<money<<" to account number: "<<AccNumber<<" on "<<dateTime<<endl;
+        transfers.close();
+    }
+     else{
+      cout << "Your account doesn't have that money";
+
+    }
+
+    cout << "Quieres continuar?";
+    cin >> opcion;
+    system("clear");
+
+
+    
+}
+
+    
+
+    return 0;
+}
+
+
+
